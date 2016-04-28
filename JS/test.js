553d3815cbc7a99950f77212dfdd1e1e3b28fb86
0qiqi0 <1483593015@qq.com>
2016-04-28 23:53:48 +0800

    regexp,date

---
+++ JS/test.js
@@ -0,0 +1,18 @@
+/**
+ * Created by dell on 2016/4/28.
+ */
+/*
+var num = new Array(3);
+console.log(num)*/
+/*
+var num = ["red","blue","black"];
+console.log(num)
+console.log(num.toString());
+console.log(num.valueOf())
+
+var s1 = num.push("pink","grown") ; console.log(s1);
+console.log(num.pop());
+
+console.log(num)*/
+var num = ["red","blue","black"];
+console.log(num.indexOf('black',1));
